b3abbefc75a636ee285f81c27501baffefe872c6
Justin Farooq <justinfarooq@gmail.com>
2015-02-15 21:01:45 -0800

    push to commit

--- heap/heap.rb
+++ heap/heap.rb
@@ -9,13 +9,35 @@ class MinHeap
 		@store = []
 	end
 
+	def count
+		@store.size
+	end
+
+	def extract
+		raise "nothing to extract" if count == 0
+
+		value = @store.pop
+		heapify_down(0) unless count == 0
+
+		value
+	end
+
 	def push(val)
 		@store << val
-		# heapify_up
+		heapify_up(count - 1)
+	end
+
+	def ==(other_heap)
+		@store == other_heap.store
 	end
 
 	def child_indices(parent_idx)
-		[2 * parent_idx + 1, 2 * parent_idx + 2].select{ |idx| idx < @store.size }
+		[2 * parent_idx + 1, 2 * parent_idx + 2].select{ |idx| idx < count }
+	end
+
+	def parent_index(child_idx)
+		raise "root has no parent" if child_idx == 0
+		(child_idx - 1) / 2
 	end
 
 	def heapify_down(parent_idx)
@@ -27,7 +49,7 @@ class MinHeap
 		# careful when there is only 1 child
 		child_indices.each do |idx|
 			child = @store[idx]
-			children << child if child >= parent
+			children << child if child < parent
 		end
 
 		# recursive exit
@@ -48,17 +70,45 @@ class MinHeap
 		heapify_down(swap_idx)
 	end
 
-	def pretty_print
-		ap to_hash
+	def heapify_up(child_idx)
+		return if child_idx == 0
+
+		parent_idx = parent_index(child_idx)
+		child, parent = @store[child_idx], @store[parent_idx]
+
+		if child >= parent
+			return
+		else
+			@store[child_idx], @store[parent_idx] = parent, child
+			heapify_up(parent_idx)
+		end
 	end
 end
 
 #ad hoc testing
-a = [16, 14, 10, 8, 7, 9, 3, 2, 4, 1].reverse!
+# a = [16, 14, 10, 8, 7, 9, 3, 2, 4, 1].reverse!
 heap = MinHeap.new
+# a.each { |el| heap.push(el) }
 
-a.each do |el|
-	heap.push(el)
-end
+# puts "heap before heapifying"
+# puts heap; puts
 
+# puts "heap after heapifying"
+# h1 = heap.heapify_down(1)
+# h2 = heap.heapify_up(3)
+# puts h1 == h2
+# # puts heap
+
+# b = %w(5 7 3 9 1).map(&:to_i)
+# # puts b
+# heap = MinHeap.new
+# b.each { |el| heap.push(el) }
+# # puts heap
+
+# c = (0..4).map { heap.extract }
+# puts c
+# puts heap.store
+
+a = ["e", "a", "s", "y", "q", "u", "e", "s", "t", "i", "o", "n"]
+a.each{|char| heap.push(char) }
 puts heap
