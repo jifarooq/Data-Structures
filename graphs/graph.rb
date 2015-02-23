dbab99fb5645e34a343fc96d42982835809ab1db
Justin Farooq <justinfarooq@gmail.com>
2015-02-22 21:07:33 -0800

    topological sort

---
+++ graphs/graph.rb
@@ -0,0 +1,39 @@
+class Node
+	attr_reader :value, :in_edges, :out_edges
+
+	def initialize(value)
+		@value = value
+		@in_edges = []
+		@out_edges = []
+	end
+
+	def to_s
+		value
+	end
+end
+
+# don't forget the cost!
+class Edge
+	attr_accessor :from_node, :to_node, :cost
+
+	def initialize(from_node, to_node, cost = 1)
+		@from_node = from_node
+		@to_node = to_node
+		@cost = cost
+
+		to_node.in_edges << self
+		from_node.out_edges << self
+	end
+
+	def destroy
+		to_node.out_edges.delete(self)
+		from_node.in_edges.delete(self)
+		to_node = nil
+		from_node = nil
+	end
+
+	def to_s
+		[from_node, to_node].map(&:to_s)
+	end
+end
+
---
+++ graphs/topological_sort.rb
@@ -0,0 +1,46 @@
+require_relative 'graph'
+
+def topological_sort(nodes)
+	in_edge_counts = {}
+	queue = []
+	sorted_nodes = []
+
+	# queue begins with all 'central' nodes
+	nodes.each do |node|
+		in_edge_counts[node.value] = node.in_edges.size
+		queue << node if node.in_edges.empty?
+	end
+
+	until queue.empty?
+		node = queue.shift
+		sorted_nodes << node
+
+		# to_node is destination point
+		node.out_edges.each do |edge|
+			to_node = edge.to_node
+
+			in_edge_counts[to_node.value] -= 1
+			queue << to_node if in_edge_counts[to_node.value] == 0
+		end
+	end
+
+	sorted_nodes
+end
+
+#TESTING
+nodes = []
+nodes << (v1 = Node.new("Wash Markov"))
+nodes << (v2 = Node.new("Feed Markov"))
+nodes << (v3 = Node.new("Dry Markov"))
+nodes << (v4 = Node.new("Brush Markov"))
+Edge.new(v1, v2)
+Edge.new(v1, v3)
+Edge.new(v2, v4)
+Edge.new(v3, v4)
+
+vals = nodes.map{ |v| v.to_s }
+
+sorted_nodes = topological_sort(nodes.shuffle)
+sorted_vals = sorted_nodes.map{ |v| v.to_s }
+
+puts "sorted nodes are #{sorted_vals}"
